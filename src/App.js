7c761f4b11711d6dcd599faecd662874817f4dd8
aman sahota <amansahota1996@gmail.com>
2019-11-10 14:05:57 +0530

    github-finder

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
+import Search from './components/users/Search';
+
+class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  };
+
+  // //Loading github users
+  // async componentDidMount() {
+  //   this.setState({ loading: true });
+  //   const res = await axios.get('https://api.github.com/users');
+  //   this.setState({ users: res.data, loading: false });
+  // }
+
+  //Searching github users
+  searchUsers = async term => {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${term}`
+    );
+    this.setState({ users: res.data.items, loading: false });
+  };
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <div className="container">
+          <Search searchUsers={this.searchUsers} />
+          <Users loading={this.state.loading} users={this.state.users} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
