db9be8e138d69411b49d8b2433ea18cf4440405c
aman sahota <amansahota1996@gmail.com>
2019-11-10 20:30:47 +0530

    Added Alert

--- src/App.js
+++ src/App.js
@@ -5,19 +5,14 @@ import './App.css';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
+import Alert from './components/layout/Alert';
 
 class App extends Component {
   state = {
     users: [],
-    loading: false
+    loading: false,
+    alert: null
   };
-
-  // //Loading github users
-  // async componentDidMount() {
-  //   this.setState({ loading: true });
-  //   const res = await axios.get('https://api.github.com/users');
-  //   this.setState({ users: res.data, loading: false });
-  // }
 
   //Searching github users
   searchUsers = async term => {
@@ -33,15 +28,24 @@ class App extends Component {
     this.state.users.length > 0 && this.setState({ users: [] });
   };
 
+  //Set Alert
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type } });
+
+    setTimeout(() => this.setState({ alert: null }), 3000);
+  };
+
   render() {
     return (
       <div className="App">
         <Navbar />
         <div className="container">
+          <Alert alert={this.state.alert} />
           <Search
             searchUsers={this.searchUsers}
             clearUsers={this.clearUsers}
             showClear={this.state.users.length > 0 ? true : false}
+            setAlert={this.setAlert}
           />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -2,11 +2,19 @@ import React, { Component } from 'react';
 
 class Search extends Component {
   state = { text: '' };
+
   onChange = e => this.setState({ [e.target.name]: e.target.value });
+
   onSubmit = e => {
     e.preventDefault();
-    this.props.searchUsers(this.state.text);
+    if (this.state.text === '') {
+      this.props.setAlert('Please Enter Something', 'light');
+    } else {
+      this.props.searchUsers(this.state.text);
+      this.setState({ text: '' });
+    }
   };
+
   render() {
     return (
       <div>
